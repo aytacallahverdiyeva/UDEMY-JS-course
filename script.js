285daf3f2c519c0ca98374f17e21369e2efa0951
aytacallahverdiyeva <you@example.com>
2024-03-06 22:42:05 +0400

    values and variables

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-let js = "amazing";
-      if (js === "amazing") alert("JS is funn");
+// let js = "amazing";
+//       if (js === "amazing") alert("JS is funn");
 
-      2 + 3 - 2 + 10;
-      console.log(2 + 3 - 2 + 10);
+//       2 + 3 - 2 + 10;
+//       console.log(2 + 3 - 2 + 10);
+
+// VALUES AND VARIABLES
+
+console.log('Aytac');
+console.log(24);
+
+let userName = 'Aytac';
+console.log(userName);
+
+// naming variables
+// DO
+// let personName = 'aytac';
+// let person_name = 'aytac';
+// let _foo = "lorem";
+// let $foo = "loremmm";
+
+// DON'T
+// let 3years = 3;
+// let aytac&name = 'aytac';
+// let new = "blablabla";
+// let function = "lorem";
+// let Person = "Sara";
+
+// we can use this one but in some cases this creates some problems
+// let name = "Aytac";
