13a59d0c1928966f90c63fcbe6308ef0e9b16fa2
aytacallahverdiyeva <aytac.allahverdiyeva01@gmail.com>
2024-03-07 11:55:14 +0400

    data types and created assignments.js file for tasks

---
+++ assignments.js
@@ -0,0 +1,10 @@
+/* 
+^ First assignment values and variables
+*/
+// let country = 'Azerbaijan';
+// let continent = 'Europe';
+// let population = 10446000;
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
--- script.js
+++ script.js
@@ -4,22 +4,28 @@
 //       2 + 3 - 2 + 10;
 //       console.log(2 + 3 - 2 + 10);
 
-// VALUES AND VARIABLES
+/*
+? VALUES AND VARIABLES 
+*/
 
-console.log('Aytac');
-console.log(24);
+// console.log('Aytac');
+// console.log(24);
 
-let userName = 'Aytac';
-console.log(userName);
+// let userName = 'Aytac';
+// console.log(userName);
 
 // naming variables
-// DO
+/* 
+* DO
+*/
 // let personName = 'aytac';
 // let person_name = 'aytac';
 // let _foo = "lorem";
 // let $foo = "loremmm";
 
-// DON'T
+/* 
+! DON'T 
+*/
 // let 3years = 3;
 // let aytac&name = 'aytac';
 // let new = "blablabla";
@@ -27,8 +33,28 @@ console.log(userName);
 // let Person = "Sara";
 
 
-// we can use this one but in some cases this creates some problems
+/*
+^ we can use this one but in some cases this creates some problems
+ */
 // let name = "Aytac";
-
 // let firstName = "aytacka";
 // console.log(firstName);
+
+
+/*
+? DATA TYPES
+*/
+
+let jsIsFun = true;
+console.log(jsIsFun);
+console.log(typeof jsIsFun);
+
+jsIsFun = "yes sure!";
+console.log(typeof jsIsFun);
+
+// undefined data type
+let age;
+console.log(age); //it gives us undefined 
+console.log(typeof age); //it also gives us undefined
+
+console.log(typeof null); //output object (it is a bug)
