818f16719e5cc67037b05d67d6e000d964ccd425
aytacallahverdiyeva <aytac.allahverdiyeva01@gmail.com>
2024-03-14 22:04:29 +0400

    switch statements task

--- assignments.js
+++ assignments.js
@@ -125,7 +125,29 @@
 // 	console.log('Someone sholud drive');
 // }
 
+// & Switch statements
 
+// const language = 'hindi';
+
+// switch (language) {
+//     case 'chinese': 
+//     case 'mandarin':
+//         console.log('Most number of native !');
+//     break;
+//     case 'spanish':
+//         console.log('2nd place in number of native speakers');
+//     break;
+//     case 'english':
+//         console.log('3rd place');
+//     break;
+//     case 'hindi':
+//         console.log('Number 4');
+//     break;
+//     case 'arabic':
+//         console.log('5th most spoken language');
+//     break;
+//     default: console.log('Great language too :D');
+// }
 
 
 
