d7488d67b3c63e4a34c84e77642903c7ccf0f7bd
aytacallahverdiyeva <aytac.allahverdiyeva01@gmail.com>
2024-03-16 00:05:59 +0400

    strict mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,13 @@
+'use strict'; 
+
+// * Strict mode 
+// let hasDriverLicense = false;
+// const passTest = true;
+
+// if(passTest) hasDriverLicense = true;
+// if(hasDriverLicense) console.log('i can drive');
+
+//strict mode reservs some words so we cannot use them for variables
+// const interface = 'Audio';
+// const private = 534;
+
